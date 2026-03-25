bf210f7822ac9a228f999d0c8709bfee7005ef22
Tejaswi VC <tejaswivemala@gmail.com>
2026-03-25 22:26:00 +0530

    fixed branch issue

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,7 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Java CI/CD Application Started...");
         
-        while (true) {
+        for (int i = 0; i < 5; i++) {
             Thread.sleep(5000);
             System.out.println("Hello from Java CI/CD Pipeline!");
         }
